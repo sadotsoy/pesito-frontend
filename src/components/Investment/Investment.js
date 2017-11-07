ba06a131f85e13e5072cb9b864e42b141b098c8e
Sadot Cortes <sadotcortes@scoorm.me>
2017-11-07 13:49:52 -0600

    Investment component created && added

---
+++ src/components/Investment/Investment.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Investment extends Component {
+
+  render() {
+    return(
+      <div className="Investment">
+        <h1>Investment</h1>
+      </div>
+    );
+  }
+}
+
+export default Investment;
