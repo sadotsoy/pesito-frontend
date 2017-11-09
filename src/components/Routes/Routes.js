d5869f16dd2b37af0db02db79d83536f0035ef6a
HomeroValdovinos <homero.valdovinos.toledo@gmail.com>
2017-11-08 21:27:49 -0600

    Routes Login page

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Home from '../Home/Home';
+import Login from '../Login/Login';
 import Users from '../Users/Users';
 import Introduction from '../Introduction/Introduction';
 import Investment from '../Investment/Investment';
@@ -10,6 +11,7 @@ class Routes extends Component {
     return(
         <Switch>
           <Route exact path="/" component = {Home} />
+            <Route path = "/login" component = {Login} />
             <Route path = "/users" component = {Users} />
             <Route path = "/introduction" component = {Introduction} />
             <Route path = "/invest" component = {Investment} />
